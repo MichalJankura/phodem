37a83f72d68d5297c1b8508dcc60f3b1757d2e48
Michal Jankura <michal.jankura@gmail.com>
2025-07-26 18:59:31 +0200

    Phone fix MH

--- src/components/Mainhero.tsx
+++ src/components/Mainhero.tsx
@@ -1,4 +1,5 @@
 import heroBg from '../assets/hero-bg-2.png';
+import pohneBg from '../assets/hero-bg-phone.png';
 import FirefliesOverlay from "../components/FirefliesOverlay";
 
 const Mainhero = () => {
@@ -7,10 +8,17 @@ const Mainhero = () => {
       className="absolute inset-0 flex items-center justify-center"
     >
       <FirefliesOverlay/>
+      {/* Desktop/Tablet background */}
       <img
         src={heroBg}
         alt="Hero Image"
-        className="w-full h-full object-cover object-center"
+        className="hidden sm:block w-full h-full object-cover object-center"
+      />
+      {/* Phone background */}
+      <img
+        src={pohneBg}
+        alt="Hero Image"
+        className="block sm:hidden w-full h-full object-cover object-center"
       />
     </div>
   );
